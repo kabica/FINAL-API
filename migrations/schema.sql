b80671da0b368558a3761d117e3cbffb74241f7f
Kevin Alexander <kalex.bica@gmail.com>
2020-03-13 11:58:43 -0600

    change ignore layout

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,11 +1,73 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
   email VARCHAR(255),
-  pw VARCHAR(255),
-  event_id INTEGER REFERENCES events(id)
+  password VARCHAR(255),
+  nickname VARCHAR(255)
 );
+
+DROP TABLE IF EXISTS user_images CASCADE;
+CREATE TABLE user_images (
+  id SERIAL PRIMARY KEY NOT NULL,
+  avatar_source VARCHAR(255),
+  country_name VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS psn_alias CASCADE;
+CREATE TABLE psn_alias (
+  id SERIAL PRIMARY KEY NOT NULL,
+  alias VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+DROP TABLE IF EXISTS xbox_alias CASCADE;
+CREATE TABLE xbox_alias (
+  id SERIAL PRIMARY KEY NOT NULL,
+  alias VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+DROP TABLE IF EXISTS battlenet_alias CASCADE;
+CREATE TABLE battlenet_alias (
+  id SERIAL PRIMARY KEY NOT NULL,
+  alias VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+DROP TABLE IF EXISTS uplay_alias CASCADE;
+CREATE TABLE uplay_alias (
+  id SERIAL PRIMARY KEY NOT NULL,
+  alias VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+DROP TABLE IF EXISTS epic_alias CASCADE;
+CREATE TABLE epic_alias (
+  id SERIAL PRIMARY KEY NOT NULL,
+  alias VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+DROP TABLE IF EXISTS origin_alias CASCADE;
+CREATE TABLE origin_alias (
+  id SERIAL PRIMARY KEY NOT NULL,
+  alias VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+DROP TABLE IF EXISTS steam_alias CASCADE;
+CREATE TABLE steam_alias (
+  id SERIAL PRIMARY KEY NOT NULL,
+  alias VARCHAR(255),
+  user_id REFERENCES users(id)
+);
+
+
+
+
+
+
+
+
+
+
 
 DROP TABLE IF EXISTS events CASCADE;
 CREATE TABLE events (
