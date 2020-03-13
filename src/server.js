c351460f7f9ee94adeafac41f6e905796a4d04d9
Kevin Alexander <kalex.bica@gmail.com>
2020-03-13 11:00:35 -0600

    setup API server structure

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// server.js
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req , res) => {
+  res.json({status: 'API functioning as expected.'})
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT: ${PORT}`)
+});
