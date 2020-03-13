b91ca214323bcb1f0d66323fefcd51262fa889ad
Kevin Alexander <kalex.bica@gmail.com>
2020-03-13 11:03:13 -0600

    refactor server start-up

--- src/server.js
+++ src/server.js
@@ -4,17 +4,19 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 const bodyParser = require('body-parser');
-
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
+// ================================ DEVELOPMENT ================================ //
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// =============================== SERVER ROUTES =============================== //
 app.get('/', (req , res) => {
   res.json({status: 'API functioning as expected.'})
 });
