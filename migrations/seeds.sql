b93b56392fa6f7710590dc6006b498df108bd1c3
Kevin Alexander <kalex.bica@gmail.com>
2020-03-13 17:58:53 -0600

    update schema and seed data

--- migrations/seeds.sql
+++ migrations/seeds.sql
@@ -15,8 +15,8 @@ VALUES ('So my only question is: do you get me bruvv?');
 INSERT INTO comments (author_id, user_id, text)
 VALUES (1, 2, 'ALT+TAB right now please.');
 
-INSERT INTO favorite_games (user_id, game1_id, game2_id, game3_id)
-VALUES (1, 1, 2, 3);
+INSERT INTO favorite_games (user_id, game1_id, game2_id)
+VALUES (1, 1, 2);
 
 INSERT INTO user_images (avatar_source, country_name, user_id)
 VALUES ('avatar_image', 'country_image', 1);
@@ -24,29 +24,19 @@ VALUES ('avatar_image', 'country_image', 1);
 INSERT INTO user_games (user_id, game_id)
 VALUES (1, 1);
 
-INSERT INTO discord_alias (alias, user_id)
-VALUES ('Discord_Alias', 1);
+INSERT INTO alias (user_id, platform_alias)
+VALUES 
+(1, 'Discord_alias');
 
-INSERT INTO psn_alias (alias, user_id)
-VALUES ('psn_Alias', 1);
 
-INSERT INTO xbox_alias (alias, user_id)
-VALUES ('xbox_Alias', 1);
+INSERT INTO game_platforms (game_id, platform_image)
+VALUES 
+(1, 'image1'),
+(2, 'image2');
 
-INSERT INTO battlenet_alias (alias, user_id)
-VALUES ('battlenet_Alias', 1);
 
-INSERT INTO uplay_alias (alias, user_id)
-VALUES ('uplay_Alias', 1);
 
-INSERT INTO epic_alias (alias, user_id)
-VALUES ('epic_Alias', 1);
 
-INSERT INTO origin_alias (alias, user_id)
-VALUES ('origin_Alias', 1);
-
-INSERT INTO steam_alias (alias, user_id)
-VALUES ('steam_Alias', 1);
 
 
 
