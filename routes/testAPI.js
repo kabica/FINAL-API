27d5fe828297169310fbd24862a2f358be81d9f4
Kevin Alexander <kalex.bica@gmail.com>
2020-03-23 17:56:07 -0600

    add encryption for emails and setup auth route

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,10 +3,12 @@
 
 const fs = require("fs");
 const path = require("path");
-
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
 const chalk = require('chalk');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('a1b2c3d4e5f6g7h8alexa1b2c3d4e5f6g7h8');
 
 const { db } = require('../db/db_config.js');
 
@@ -33,6 +35,11 @@ const getUsers = function () {
   return db.query(`SELECT * FROM users;`)
     .then(res => res.rows);
 };
+const addUser = function(email, password) {
+  return db.query(`INSERT INTO users (email, password) VALUES($1, $2) RETURNING email;`,[email, password])
+  .then(res => res.rows)
+}
+
 const getUserImages = function () {
   return db.query(`SELECT * FROM user_images;`)
     .then(res => res.rows);
@@ -58,13 +65,19 @@ const getFavoriteGames = function () {
     .then(res => res.rows);
 };
 const getComments = function () {
-  return db.query(`SELECT * FROM comments;`)
+  return db.query(`SELECT * FROM comments ORDER BY created_at;`)
     .then(res => res.rows);
 };
 const getAlias = function () {
   return db.query(`SELECT * FROM alias;`)
     .then(res => res.rows);
 };
+const postComment = function(author, user, text) {
+  return db.query(`INSERT INTO comments (author_id, user_id, text) VALUES ($1, $2, $3);`, [author, user, text])
+  .then(res => res.rows)
+}
+
+let testObj = {};
 
 // ============================ API ROUTER ============================ //
 module.exports = () => {
@@ -136,9 +149,76 @@ module.exports = () => {
       console.log(chalk.red('error: ', error))
     });
   });
+  router.post('/comments', (req , res) => {
+    const {author , user , text} = req.body;
+    postComment(author, user, text)
+    .then(console.log('done'))
+    .catch(error => console.error(error))
+  })
 
   router.post('/alex', (req , res) => {
-    console.log('HERE: ', req.body)
+    console.log(req.body)
+  });
+
+  // USER AUTHENTICATION
+  // Payload = the encrypted email stored in Clinet sessionStorage
+  // Email is decrypted and run against the database 
+  // If there is a match, verified === true
+  router.post('/auth', async(req , res) => {
+    const decryptedString = cryptr.decrypt(req.body.email);
+    const verify = await checkEmail(decryptedString);
+    const verified = verify[0] ? 'true' : 'false'
+
+    res.json({em: decryptedString, verified: verified})
+  });
+
+  const getPassword = function(email) {
+    return db.query(`SELECT password FROM users WHERE email = $1;`, [email])
+    .then(res => res.rows)
+    .catch(error => console.log(error))
+  }
+  const checkEmail = function(email) {
+    return db.query(`SELECT email FROM users WHERE email = $1;`, [email])
+    .then(res => res.rows)
+    .catch(error => console.log(error))
+  }
+  router.post('/login', async(req , res) => {
+    let {email , password} = req.body;
+    let storedEmail = await checkEmail(email);
+    // let verifyEmail = storedEmail[0] ? true: false;
+    if(!storedEmail[0]) {
+      res.json({error: 'Invalid Email'})
+      return;
+    }
+
+    let storedPW = await getPassword(email);
+    storedPW = storedPW[0].password;
+
+    if(bcrypt.compareSync(password, storedPW)) {
+      console.log(storedEmail)
+      const encryptedEmail = cryptr.encrypt(storedEmail[0].email);
+      console.log(encryptedEmail)
+      res.json({Encryption: encryptedEmail})
+    } else {
+      res.json({error: 'Invalid Password'})
+    }
+  });
+
+  router.post('/register', async(req , res) => {
+    let {email , password} = req.body;
+    let verifyEmail = await checkEmail(email);
+    if(verifyEmail[0]) {
+      res.json({error: 'Email is already in use.'});
+      return;
+    }
+    encryptedPW = bcrypt.hashSync(password, 10)
+    addUser(email, encryptedPW)
+    .then(email => {
+      let storedEmail = email[0].email;
+      const encryptedEmail = cryptr.encrypt(storedEmail);
+      res.json({Encryption: encryptedEmail})
+    })
+    .catch(err => console.log(err))
   })
 
   
